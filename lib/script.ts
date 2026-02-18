2c43834b0dd604b721a624e5b7fbee43a7a31809
Matvei Andrienko <m.y.andrienko@outlook.com>
2026-02-18 17:26:38 +0100

    nicer syntax for scripts

--- lib/script.ts
+++ lib/script.ts
@@ -1,14 +1,31 @@
 import type { Redis } from "@upstash/redis";
 import { getRedis } from "./redis.ts";
 
-type Script<R> = ReturnType<typeof Redis.prototype.createScript<R, false>>;
+export interface Script<T extends (...args: string[]) => unknown> {
+  (keys: string[], ...args: Parameters<T>): Promise<ReturnType<T>>;
+}
 
-export function createScript<T extends (...args: string[]) => unknown>(
+export function script<T extends (...args: string[]) => unknown>(
+  template: readonly string[],
+  ...substitutions: unknown[]
+): Script<T> {
+  const src = String.raw({ raw: template }, substitutions).trim();
+  return createScript(src);
+}
+
+// Private
+
+type RedisScript<R> = ReturnType<typeof Redis.prototype.createScript<R, false>>;
+
+function createScript<T extends (...args: string[]) => unknown>(
   src: string,
-) {
-  let script: Script<ReturnType<T>>;
+): Script<T> {
+  let script: RedisScript<ReturnType<T>>;
 
-  return async function execScript(keys: string[], ...args: Parameters<T>) {
+  return async function execScript(
+    keys: string[],
+    ...args: Parameters<T>
+  ): Promise<ReturnType<T>> {
     if (!script) {
       script = getRedis().createScript(src);
     }
