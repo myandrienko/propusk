dfa447bfadc7b534d4098e336c477619ac4c776e
Matvei Andrienko <m.y.andrienko@outlook.com>
2026-02-20 23:39:09 +0100

    fix missing tg client

---
+++ lib/tg.ts
@@ -0,0 +1,12 @@
+import { Telegram } from "wrappergram";
+import { env } from "./env.ts";
+
+let tg: Telegram | undefined;
+
+export function getTg(): Telegram {
+  if (!tg) {
+    tg = new Telegram(env("BOT_TOKEN"));
+  }
+
+  return tg;
+}
