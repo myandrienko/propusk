b84865bdf0f65f7ab69a4894ceabe547db78b073
Matvei Andrienko <m.y.andrienko@outlook.com>
2026-02-27 23:52:21 +0100

    fix: async e.try bug

--- lib/try.ts
+++ lib/try.ts
@@ -1,19 +1,19 @@
-export const e = {
-  try<T>(cb: () => T, wrapErr: (err: unknown) => unknown): T {
-    const handleErr = (err: unknown): never => {
-      throw wrapErr(err) ?? err;
-    };
+function etry<T>(
+  cb: () => Promise<T>,
+  wrapErr: (err: unknown) => unknown,
+): Promise<T>;
+function etry<T>(cb: () => T, wrapErr: (err: unknown) => unknown): T;
+function etry(cb: () => unknown, wrapErr: (err: unknown) => unknown) {
+  const handleErr = (err: unknown): never => {
+    throw wrapErr(err) ?? err;
+  };
 
-    try {
-      const res = cb();
+  try {
+    const res = cb();
+    return res instanceof Promise ? res.catch(handleErr) : res;
+  } catch (err) {
+    return handleErr(err);
+  }
+}
 
-      if (res instanceof Promise) {
-        res.catch(handleErr);
-      }
-
-      return res;
-    } catch (err) {
-      return handleErr(err);
-    }
-  },
-};
+export const e = { try: etry };
